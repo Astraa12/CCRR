9a770f7510f06e281fba4b6fd956d7f417afe0bb
alex <emymorgan45@gmail.com>
2024-01-19 09:42:41 -0600

    testing render

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request
+import requests, random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('homepage.html')
+    return render_template('homepage.html', randInt = randInt)
+
+randInt = random.randint(0, 6)
